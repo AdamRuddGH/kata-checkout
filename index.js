0db4ca77f3cbf577b3c5b051c570757241312dcd
Adam Rudd <adam.rudd@myob.com>
2021-07-20 14:41:48 +1000

    initial commit with failing tests

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+class Item {
+    
+
+    constructor(itemId, itemName, itemPrice) {
+      this.itemId = itemId;
+      this.itemName = itemName;
+      this.itemPrice = itemPrice;
+    }
+    pingpong() {
+        console.log("This is the Item Class")
+    }
+  }
+
+class MultiProductDeals {
+    constructor(itemId, itemName, qty, dealType, deal) {
+        this.item = item; //item class
+        this.qty = qty;
+        this.dealType = dealType;
+        this.deal = deal;
+    }
+
+}
+
+class Purchases {
+    constructor() {
+        this.items = [];
+        this.grossPrice = 0.00;
+        this.discount = 0.00;
+        this.netPrice = 0.00;
+    }
+
+    calculateDiscount() {
+        //calculate the discount of items based on the available Deals
+
+        //check the multi product deal ids for matches to items in receipt
+        //if there is a match, check for the deal specifics.
+        //if the number of items // deal qty >=1 (floor division), 
+        //then apply the difference in price to the discount
+        originalDiscount = this.discount;
+        
+    }
+}
+
+// const my_item = new Item(1,"ss",2);
+// my_item.pingpong()
---
+++ test/example.test.js
@@ -0,0 +1,26 @@
+// const assert = import('assert');
+import * as Checkout from "index.js";
+
+// describe('Simple Math Test', () => {
+//  it('should return 2', () => {
+//         assert.strictEqual(1 + 1, 2);
+//     });
+//  it('should return 9', () => {
+//         assert.strictEqual(3 * 3, 9);
+//     });
+// });
+
+myItem2 = new Checkout.Item(1,"ss",2)  //fails for some reason '[ERR_REQUIRE_ESM]: Must use import to load ES Module:' even though i'm using import
+
+
+// describe('Item creation', () => {
+
+//     myItem = new Checkout.Item(281, "Apple", 0.50)
+
+//     it('should initialise with all fields',() => {
+        
+//         assert.strictEqual(1,1)
+//     });
+
+
+//     });
